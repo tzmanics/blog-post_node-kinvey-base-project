503a016c215dd8e41fef7ae8756d0b50aef0ac92
tzmanics <tzmanics@gmail.com>
2019-04-07 23:12:34 -0400

    adds config and config example

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 // app.js
 
 const Kinvey = require('kinvey-node-sdk');
+const config = require('./config');
 
-Kinvey.init({
-  appKey: 'kid_S16j3xVFN',
-  appSecret: 'e0a009c5a6f84949a8310e8c24ff2b7f'
-});
+Kinvey.init(config.kinvey);
 
 // optional code to test for successful connection
 Kinvey.ping().then((response) => {
---
+++ config-example.js
@@ -0,0 +1,13 @@
+// config-example.js
+// this is an example of what your config file would look like
+// your actual config file should be added to your .gitignore file
+// and never see the light of day online 😁
+
+const config ={
+  kinvey: {
+    appKey: 'kid_S16j3xVFN',
+    appSecret: 'e0a009c5a6f84949a8310e8c24ff2b7f'
+  }
+}
+
+module.exports = config;
